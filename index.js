6a9a84ecab44866e02c53c6332d5ed2103d9b261
Avinash Pathy <avinashpathy2001@gmail.com>
2023-11-19 23:39:47 +0530

    crud app using mongodb

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const {MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP} = require('./config/config');
 
+const postRouter = require("./routes/postRoutes");
+
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 const connectWithRetry = () => {
@@ -9,7 +11,6 @@ const connectWithRetry = () => {
   .connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
   })
   .then(() => console.log("successfully connected to database"))
   .catch((e) => {
@@ -22,9 +23,13 @@ connectWithRetry();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("<h2>Hi there!!!</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.port || 4000;
 
