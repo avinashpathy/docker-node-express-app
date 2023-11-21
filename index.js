8be78d36df44bf376012f2925056a28e13883ac2
Avinash Pathy <avinashpathy2001@gmail.com>
2023-11-21 23:38:11 +0530

    user signup and login

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const {MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP} = require('./config/config');
 
 const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
@@ -30,6 +31,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
 const port = process.env.port || 4000;
 
